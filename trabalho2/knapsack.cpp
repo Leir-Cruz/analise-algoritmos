29b7aa7b288d24ea77025698247b531550e629ea
Gabriel Cruz <gabrielcruzvazsantos@gmail.com>
2023-02-12 10:18:44 -0300

    [feat] first try greedy solution

--- trabalho2/knapsack.cpp
+++ trabalho2/knapsack.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 #include <vector>
+#include<bits/stdc++.h>
 using namespace std;
+
+int greedySolution (vector<int> weights, vector<int> values, vector<pair<float,int>> benefits, int capacity) {
+    int size = benefits.size();
+    int total_value = 0;
+    for(int i = 0; i < size; i++) {
+        if(capacity == 0) break;
+        int indexMaxValue = benefits[i].second;
+        if(weights[indexMaxValue] <= capacity) {
+            capacity -= weights[indexMaxValue];
+            total_value += values[indexMaxValue];
+        } else {
+            int fractionalValue = values[indexMaxValue] * (capacity / weights[indexMaxValue]);
+            total_value += fractionalValue;
+        }
+    }
+    return total_value;
+}
 
 int main() {
     int sackSize, numberOfItems;
     cin >> sackSize >> numberOfItems;
     vector<int> weightArray, valueArray;
-    vector<float> benefitArray;
+    vector<pair<float,int>> benefitArray;
 
     for(int i = 0; i < numberOfItems; i++) {
         int weight, value;
@@ -14,7 +32,7 @@ int main() {
 
         weightArray.push_back(weight);
         valueArray.push_back(value);
-        benefitArray.push_back(item/weight);
+        benefitArray.push_back(make_pair(value/weight, i));
     }
 
     int weightArraySize = weightArray.size();
@@ -23,6 +41,12 @@ int main() {
     if( valueArraySize != weightArraySize || valueArraySize != numberOfItems || weightArraySize != numberOfItems ) {
         return -1;
     }
+
+    sort(benefitArray.rbegin(), benefitArray.rend());
+    int maxValue = greedySolution(weightArray, valueArray, benefitArray, sackSize);
+
+    cout << maxValue;
+
 
     return 0;
 }
