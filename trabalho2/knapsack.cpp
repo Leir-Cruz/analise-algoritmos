dae6d869cc9bef621128304af64fe172886075d6
Gabriel Cruz <gabrielcruzvazsantos@gmail.com>
2023-02-12 08:45:54 -0300

    getting necessary inputs

--- trabalho2/knapsack.cpp
+++ trabalho2/knapsack.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <sstream>
 using namespace std;
 
 int main() {
     int sackSize, numberOfItems;
     cin >> sackSize >> numberOfItems;
+    vector<int> itemsArray, weightArray;
+
+    for(int i = 0; i < numberOfItems; i++) {
+        int item, weight;
+        cin >> item >> weight;
+
+        itemsArray.push_back(item);
+        weightArray.push_back(weight);
+    }
+
+    int itemsArraySize = itemsArray.size();
+    int weightArraySize = weightArray.size();
+
+    if( itemsArraySize != weightArraySize || itemsArraySize != numberOfItems || weightArraySize != numberOfItems ) {
+        return -1;
+    }
+
     return 0;
 }
