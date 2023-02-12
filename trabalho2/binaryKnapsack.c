142ac47f7495488500ab1c37eecb660d3b5d00ae
Gabriel Cruz <gabrielcruzvazsantos@gmail.com>
2023-02-12 12:22:00 -0300

    recursive solution binary knapsack c

--- trabalho2/binaryKnapsack.c
+++ trabalho2/binaryKnapsack.c
@@ -4,17 +4,36 @@ int max(int numberA, int numberB) {
     return numberA > numberB ? numberA : numberB;
 }
 
-int binarySolution (int weights[], int values[], int numberOfItems, int capacity) {
+int RecursiveSolution (int weights[], int values[], int numberOfItems, int capacity) {
     if (capacity == 0 || numberOfItems == 0) return 0;
     if(weights[numberOfItems -1] > capacity) {
-        return binarySolution(weights, values, numberOfItems - 1, capacity);
+        return RecursiveSolution(weights, values, numberOfItems - 1, capacity);
     } else {
         return max(
                 values[numberOfItems - 1] +
-                binarySolution(weights, values, numberOfItems - 1, capacity - weights[numberOfItems - 1]),
-                binarySolution(weights, values, numberOfItems - 1, capacity)
+                RecursiveSolution(weights, values, numberOfItems - 1, capacity - weights[numberOfItems - 1]),
+                RecursiveSolution(weights, values, numberOfItems - 1, capacity)
         );
     }
+}
+
+int DinamycSolution(int weights[], int values[], int numberOfItems, int capacity) {
+    int table[numberOfItems + 1][capacity +1];
+
+    for(int i = 0; i <= numberOfItems; i++) {
+        for(int c = 0; c <= capacity; c++) {
+            if(i == 0 || c == 0) table[i][c] = 0;
+            else if (weights[i - 1] <= c ) {
+                table[i][c] = max(
+                        values[i - 1] + table[i -1][c - weights[i -1]],
+                        table[i - 1][c]
+                );
+            } else {
+                table[i][c] = table[i - 1][c];
+            }
+        }
+    }
+    return table[numberOfItems][capacity];
 }
 
 int main() {
@@ -40,7 +59,7 @@ int main() {
         return -1;
     }
 
-    int maxValue = binarySolution(weightArray, valueArray, numberOfItems, sackSize);
+    int maxValue = DinamycSolution(weightArray, valueArray, numberOfItems, sackSize);
     printf("%d", maxValue);
 
     return 0;
