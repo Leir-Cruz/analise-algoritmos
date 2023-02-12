a4d8c3e116661647681109733d696579583261de
Gabriel Cruz <gabrielcruzvazsantos@gmail.com>
2023-02-11 21:09:40 -0300

    initial setup knapsack

---
+++ trabalho2/knapsack.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+int main() {
+    int sackSize, numberOfItems;
+    cin >> sackSize >> numberOfItems;
+    return 0;
+}
